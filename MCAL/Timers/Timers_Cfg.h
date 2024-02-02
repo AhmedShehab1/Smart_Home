8dd17dbb3a1d5dda79b612c0c921d6037948e02f
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-02-02 22:07:07 +0200

    Added New Files

---
+++ MCAL/Timers/Timers_Cfg.h
@@ -0,0 +1,24 @@
+/*
+ * Timers_Cfg.h
+ *
+ *  Created on: ?? ???? ?????? ???? ??
+ *      Author: Acer
+ */
+
+#ifndef INCLUDE_MCAL_TIMERS_TIMERS_CFG_H_
+#define INCLUDE_MCAL_TIMERS_TIMERS_CFG_H_
+
+#define TIMER0_MODE FAST_PWM_MODE
+
+#define TIMER0_CLK 0b010
+
+#define OCR0_VALUE 99
+
+#define CTC_OC0_MODE 0b00
+
+
+#define FAST_PWM_OC0_MODE 3
+
+
+
+#endif /* INCLUDE_MCAL_TIMERS_TIMERS_CFG_H_ */
