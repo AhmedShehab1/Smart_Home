156c5b74a6a1a0ec33ed79ddfbec98ffb41e17a3
Ahmed Shehab <142121665+AhmedShehab1@users.noreply.github.com>
2024-02-12 11:43:47 +0300

    Updated App File

--- Src/app.c
+++ Src/app.c
@@ -14,6 +14,8 @@
 #include "../Include/MCAL/UART/UART_Interface.h"
 #include "../Include/MCAL/TWI/TWI_interface.h"
 #include "../Include/MCAL/Timers/Timers_Interface.h"
+#include "../Include/MCAL/ADC/ADC_Interface.h"
+#include "../Include/MCAL/GI/GI_interface.h"
 
 #include "../Include/HAL/LED/LED_Interface.h"
 #include "../Include/HAL/EEPROM/EEPROM_interface.h"
@@ -22,6 +24,50 @@
 #include <string.h>
 #define F_CPU 8000000UL
 
+
+
+
+
+
+
+
+
+
+
+u32 global_u32CheckTime;
+void Timer2_CTC_ISR(void)
+{
+	global_u32CheckTime++;
+}
+void ADC_ISR(void)
+{
+	u16 local_u16DigitalValue, local_u16AnalogValue;
+	local_u16DigitalValue = MADC_u16GetDigitalValue(ADC3);
+	local_u16AnalogValue = (local_u16DigitalValue * 500UL) / 1024;
+	if (local_u16AnalogValue >= 0 && local_u16AnalogValue < 256)
+	{
+		MTIMER0_voidSetOCR0Value(local_u16AnalogValue);
+		_delay_ms(200);
+	}
+	if (global_u32CheckTime == Five_Minutes)
+	{
+		global_u32CheckTime = 0;
+		local_u16DigitalValue = MADC_u16GetDigitalValue(ADC2);
+		local_u16AnalogValue = (local_u16DigitalValue * 500) / 1024;
+		if (local_u16AnalogValue < 20)
+		{
+			MTIMER1_voidSetOCR1BValue(0);
+		}
+		else if (local_u16AnalogValue >= 20 && local_u16AnalogValue < 25)
+		{
+			MTIMER1_voidSetOCR1BValue(9000);
+		}
+		else if (local_u16AnalogValue >= 25)
+		{
+			MTIMER1_voidSetOCR1BValue(19999);
+		}
+	}
+}
 u8 Pre_Defined_Users [10][2][12] = {
 								 {"Ahmed", "Shehab"},
 								 {"Ahmed", "Reda"},
@@ -37,9 +83,15 @@ u8 Pre_Defined_Users [10][2][12] = {
 
 int main(void)
 {
-	u8 local_u8ReceivedData, Provided_Data[12], i, User_Exists, Password_Incorrect = 4, Flag = 0;
+	u8 local_u8ReceivedData = 254, Provided_Data[12], i, User_Exists, Password_Incorrect = 4, Flag = 0;
 	MDIO_voidInit();
 	MUART_voidInit();
+	MGI_voidEnable();
+	MADC_voidSetCallBack(ADC_ISR);
+	MTIMER0_voidInit();
+	MTIMER2_voidInit();
+	MTIMER2_voidSetCTCCallBack(Timer2_CTC_ISR);
+	MADC_voidInit();
 	MTIMER1_voidInit();
 	while(1)
 	{
@@ -97,14 +149,14 @@ int main(void)
 						{
 							if (Flag == 0)
 							{
-								MTIMER1_voidSetOCR1AValue(2500);
+								MTIMER1_voidSetOCR1AValue(2000);
 								Flag = 1;
 								local_u8ReceivedData = MUART_u8ReadByteSyncBlocking();
 								continue;
 							}
 							if (Flag == 1)
 							{
-								MTIMER1_voidSetOCR1AValue(500);
+								MTIMER1_voidSetOCR1AValue(1000);
 								Flag = 0;
 							}
 							local_u8ReceivedData = MUART_u8ReadByteSyncNonBlocking();
@@ -130,6 +182,7 @@ int main(void)
 				MDIO_voidSetPinValue(DIO_PORTA, PIN1, DIO_SET);
 				_delay_ms(2000);
 				MDIO_voidSetPinValue(DIO_PORTA, PIN1, DIO_RESET);
+				_delay_ms(1200);
 				Password_Incorrect = 4;
 			}
 		}
